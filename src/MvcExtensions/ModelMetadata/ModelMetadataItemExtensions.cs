1b18450129c302d2701ef5cd0fd42f479f52ea09
Alexander Zaytsev <hazzik@gmail.com>
2011-06-28 02:12:16 +0600

    ModelMetadataItemExtensions now public
    --HG--
    extra : transplant_source : N%D6%FD%85%F8%AC%3D%DCha%81%22%0E%7F%BC%B4F3%FF%F0

--- src/MvcExtensions/ModelMetadata/ModelMetadataItemExtensions.cs
+++ src/MvcExtensions/ModelMetadata/ModelMetadataItemExtensions.cs
@@ -1,5 +1,5 @@
 ﻿#region Copyright
-// Copyright (c) 2009 - 2010, Kazi Manzur Rashid <kazimanzurrashid@gmail.com>.
+// Copyright (c) 2009 - 2011, Kazi Manzur Rashid <kazimanzurrashid@gmail.com>.
 // This source is subject to the Microsoft Public License. 
 // See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL. 
 // All other rights reserved.
@@ -9,8 +9,18 @@ namespace MvcExtensions
 {
     using System.Linq;
 
-    internal static class ModelMetadataItemExtensions
+    /// <summary>
+    /// Defines an static class which contains extension methods of <see cref="ModelMetadataItem"/>.
+    /// </summary>
+    public static class ModelMetadataItemExtensions
     {
+        /// <summary>
+        /// Returns model validation metadata of type <typeparamref name="TValidationMetadata"/> associated with this <paramref name="item"/>. 
+        /// New validation will be created if no one is found. 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <typeparam name="TValidationMetadata"></typeparam>
+        /// <returns>Model validation metadata of type <typeparamref name="TValidationMetadata"/></returns>
         public static TValidationMetadata GetValidationOrCreateNew<TValidationMetadata>(this ModelMetadataItem item)
             where TValidationMetadata : class, IModelValidationMetadata, new()
         {
@@ -25,12 +35,25 @@ namespace MvcExtensions
             return validation;
         }
 
+        /// <summary>
+        /// Returns model validation metadata of type <typeparamref name="TValidationMetadata"/> associated with this <paramref name="item"/> or null.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <typeparam name="TValidationMetadata"></typeparam>
+        /// <returns>Model validation metadata of type <typeparamref name="TValidationMetadata"/> or null</returns>
         public static TValidationMetadata GetValidation<TValidationMetadata>(this ModelMetadataItem item)
             where TValidationMetadata : IModelValidationMetadata
         {
             return item.Validations.OfType<TValidationMetadata>().SingleOrDefault();
         }
 
+        /// <summary>
+        /// Returns model metadata additional setting of type <typeparamref name="TSetting"/> associated with this <paramref name="item"/>. 
+        /// New model setting will be created if no one is found. 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <typeparam name="TSetting"></typeparam>
+        /// <returns>Model validation metadata of type <typeparamref name="TSetting"/></returns>
         public static TSetting GetAdditionalSettingOrCreateNew<TSetting>(this ModelMetadataItem item)
             where TSetting : class, IModelMetadataAdditionalSetting, new()
         {
@@ -45,6 +68,12 @@ namespace MvcExtensions
             return setting;
         }
 
+        /// <summary>
+        /// Returns model metadata additional setting of type <typeparamref name="TSetting"/> associated with this <paramref name="item"/> or null. 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <typeparam name="TSetting"></typeparam>
+        /// <returns>Model validation metadata of type <typeparamref name="TSetting"/> or null</returns>
         public static TSetting GetAdditionalSetting<TSetting>(this ModelMetadataItem item)
             where TSetting : IModelMetadataAdditionalSetting
         {
