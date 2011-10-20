8691b4b77bfc0f8b8b28c6151b72a1c5a2917aac
Moses <none@none>
2011-10-20 23:17:11 +0800

    Updated Autofac reference to Autofac v2.5.2 for .Net 4.0 Updated MvcExtensions.Autofac & MvcExtensions.Autofac.Tests  projects Add Autofac projects back to MvcExtensions.sln solution Created new build files MvcExtensions.noncover.build & build.noncover.bat to exclude ncover from build and also added MvcExtensions.Autofac project to build outputs

---
+++ build.noncover.bat
@@ -0,0 +1,9 @@
+if "%1%" == "" goto release
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild MvcExtensions.noncover.build /t:Full /p:Configuration=%1 /m:2
+goto end
+
+:release
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild MvcExtensions.noncover.build /t:Full /p:Configuration=release /m:2
+
+:end
+pause
