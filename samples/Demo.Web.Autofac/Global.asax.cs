fda3ea763f2107726803d7de2f6cd3198ee97be7
Alexander Zaytsev <hazzik@gmail.com>
2011-10-24 22:41:14 +0600

    Added back autofac sample application into full solution.

--- samples/Demo.Web.Autofac/Global.asax.cs
+++ samples/Demo.Web.Autofac/Global.asax.cs
@@ -1,8 +1,19 @@
 ﻿namespace Demo.Web.Autofac
 {
+    using MvcExtensions;
     using MvcExtensions.Autofac;
 
     public class MvcApplication : AutofacMvcApplication
     {
+        public MvcApplication()
+        {
+            Bootstrapper.BootstrapperTasks
+                        .Include<RegisterModelMetadata>()
+                        .Include<RegisterControllers>()
+                        .Include<ConfigureFilters>()
+                        .Include<ConfigureModelBinders>()
+                        .Include<RegisterRoutes>()
+                        .Include<RegisterActionInvokers>();
+        }
     }
 }
