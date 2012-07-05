b3511981121904b67dfafe02cae2229776b19c87
hazzik <hazzik@gmail.com>
2012-07-05 23:48:21 +0600

    Restore solution level packages

--- build.bat
+++ build.bat
@@ -1,3 +1,13 @@
+@echo off
+pushd .nuget
+
+echo Restoring nuget packages
+
+call nuget install packages.config -o ..\packages
+
+popd
+
+
 set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild
 set configuration=%1
 if "%configuration%" == "" set configuration=Release
