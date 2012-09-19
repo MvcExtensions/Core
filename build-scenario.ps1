4e08ec8828f55f555671677c7277b60bc179ebc7
Alexander I. Zaytsev <hazzik@gmail.com>
2012-09-20 00:49:00 +0600

    Add MvcExtensions.FluentMetadata assembly to build script

--- build-scenario.ps1
+++ build-scenario.ps1
@@ -6,9 +6,8 @@ properties {
 	$buildNumber = "3.0.0"
 	$version = "$buildNumber.0"
 	$semVer = $buildNumber
-	$xunit = "$projectDir\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.x86.exe"
-	$corePath = "$projectDir\src\MvcExtensions"
-	$coreFile = "MvcExtensions"
+	$xunit = "$projectDir\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe"
+	$projects = @("MvcExtensions", "MvcExtensions.FluentMetadata")
 	$nuget = "$projectDir\.nuget\nuget"
 	$referencePath = "$projectDir\References"
 	$coverageRunner = "$projectDir\build\PartCover\partcover.exe"
@@ -26,7 +25,7 @@ task Init {
 }
 
 task Clean {
-	exec { msbuild $solution /t:Clean /p:Configuration=$configuration /m }
+	msbuild $solution /t:Clean /p:Configuration=$configuration /m
 }
 
 #http://sourceforge.net/projects/stylecopcli/
@@ -36,72 +35,89 @@ task StyleCop {
 
 task Simian {
 	Copy-Item $projectDir\Build\Simian\simian.xsl $artifactPath
-	& "$projectDir\Build\Simian\simian-2.2.24.exe" `
-		-formatter=xml:"$artifactPath\Simian.xml" `
-		-reportDuplicateText+ `
-		"$corePath\**\*.cs"
+	$projects | ForEach-Object {
+		$out = "$artifactPath\Simian$_.xml"
+		
+		& "$projectDir\Build\Simian\simian-2.2.24.exe" `
+			-formatter=xml:"$out" `
+			-reportDuplicateText+ `
+			"$projectDir\src\$_\**\*.cs"
+	}
 }
 
 task Build {
 	Generate-Assembly-Info `
         -outputFile "$projectDir\src\SharedFiles\CommonAssemblyInfo.cs" `
         -company "MvcExtensions" `
-        -copyright "Copyright © Kazi Manzur Rashid 2009-2012, hazzik 2011-2012" `
+        -copyright "Copyright &copy; Kazi Manzur Rashid 2009-2012, hazzik 2011-2012" `
         -comVisible "false" `
         -version $version `
         -fileVersion $version `
         -informationalVersion $semVer
 
-	exec { msbuild $solution /t:Build /p:Configuration=$configuration /m }
+	msbuild $solution /t:Build /p:Configuration=$configuration /m
 }
 
 task FxCop {
-	$fxCopOutput = "$artifactPath\FxCop.xml"
 	$fxCopTotalErrors = 0
-
 	Copy-Item $projectDir\Build\FxCop\Xml\FxCopReport.xsl $artifactPath
 
-	exec { & "$projectDir\Build\FxCop\FxCopCmd.exe" `
-		/f:"$corePath\bin\$configuration\$coreFile.dll" `
-		/d:"$referencePath\AspNetMvc" `
-		/d:"$referencePath\Autofac" `
-		/d:"$referencePath\Castle" `
-		/d:"$referencePath\Ninject" `
-		/d:"$referencePath\PnP" `
-		/d:"$referencePath\StructureMap" `
-		/dic:"$projectDir\src\SharedFiles\CodeAnalysisDictionary.xml" `
-		/o:"$fxCopOutput" `
-		/oxsl:"FxCopReport.xsl" `
-		/to:0 /fo /gac /igc /q
-	}
+	$projects | ForEach-Object { 
+		$out = "$artifactPath\FxCop$_.xml"
 
-	[xml]$xd = Get-Content $fxCopOutput
-	$nodelist = $xd.selectnodes("//Issue")
-	$fxCopTotalErrors = $nodelist.Count
-	if($fxCopTotalErrors -gt 0){
-		Write-Host "FxCop encountered $fxCopTotalErrors rule violations"
+		& "$projectDir\Build\FxCop\FxCopCmd.exe" `
+			/f:"$projectDir\src\$_\bin\$configuration\$_.dll" `
+			/d:"$referencePath\AspNetMvc3" `
+			/dic:"$projectDir\src\SharedFiles\CodeAnalysisDictionary.xml" `
+			/o:"$out" `
+			/oxsl:"FxCopReport.xsl" `
+			/to:0 /fo /gac /igc /q
+
+		[xml]$xd = Get-Content $out
+		$nodelist = $xd.selectnodes("//Issue")
+		$fxCopTotalErrors = $nodelist.Count
+		if ($fxCopTotalErrors -gt 0){
+			Write-Host "FxCop encountered $fxCopTotalErrors rule violations"
+		}	
 	}
 }
 
 task CodeCoverage {	
 	Copy-Item $projectDir\Build\PartCover\partcover.xml $artifactPath
-	exec { & $coverageRunner --register --settings $artifactPath\partcover.xml --output $artifactPath\$coreFile-coverage.xml }
+	$projects | ForEach-Object { 
+		& $coverageRunner --register --settings $artifactPath\partcover.xml --output $artifactPath\$_-coverage.xml 
+	}
 }
 
 task Tests -depends Init {
-	exec { & $xunit "$corePath.Tests\bin\$configuration\$coreFile.Tests.dll" /noshadow /xml $artifactPath\$coreFile.Tests.xunit.xml }
+	$projects | ForEach-Object {
+		& $xunit "$projectDir\src\$_.Tests\bin\$configuration\$_.Tests.dll" /noshadow /xml $artifactPath\$_.Tests.xunit.xml
+	}
 }
 
-task Deploy {
+task Deploy -depends Deploy-Zip, Deploy-Nuget  
+
+task Deploy-Zip {
 	Import-Module ".\build\PScx"
-	$files = @("$projectDir\license.txt")
-	Get-ChildItem -Path "$corePath\bin\$configuration" | Where-Object { $_.name -match "$coreFile.*" } | ForEach-Object { $files += $_.FullName }
-	Write-Zip -Path $files -OutputPath "$artifactPath\$coreFile.$semVer.zip" -level 9
-	exec { & "${nuget}.exe" pack $corePath\$coreFile.nuspec /basepath $corePath\bin\$configuration /outputdirectory $artifactPath /version $semVer }
+
+	$projects | ForEach-Object {
+		$f = $_
+		$files = @("$projectDir\license.txt")
+		Get-ChildItem -Path "$projectDir\src\$f\bin\$configuration" | Where-Object { $_.name -match "$f.*" } | ForEach-Object { $files += $_.FullName }
+		Write-Zip -Path $files -OutputPath "$artifactPath\$f.$semVer.zip" -level 9
+	}
+}
+
+task Deploy-Nuget {
+	$projects | ForEach-Object { 
+		& "${nuget}.exe" pack $projectDir\src\$_\$_.nuspec /basepath $projectDir\src\$_\bin\$configuration /outputdirectory $artifactPath /version $semVer 
+	}
 }
 
 task Publish {
-	exec { & "${nuget}.cmd" push $artifactPath\$coreFile.$semVer.nupkg }
+	$projects | ForEach-Object { 
+		& "${nuget}.cmd" push "$artifactPath\$_.$semVer.nupkg" 
+	}
 }
 
 #https://github.com/ayende/rhino-mocks/blob/master/psake_ext.ps1
