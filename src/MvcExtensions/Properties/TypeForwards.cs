d37c4f5423ebe5844c02bb9f9e6ff4c69cf0c0e2
Alexander I. Zaytsev <hazzik@gmail.com>
2012-10-17 00:00:16 +0600

    Add type forwards from MvcExtensions to MvcExtensions.FluentMetadata

---
+++ src/MvcExtensions/Properties/TypeForwards.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright
+// Copyright (c) 2009 - 2012, Kazi Manzur Rashid <kazimanzurrashid@gmail.com>, 2011 - 2012 hazzik <hazzik@gmail.com>.
+// This source is subject to the Microsoft Public License. 
+// See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL. 
+// All other rights reserved.
+#endregion
+
+using System.Runtime.CompilerServices;
+using MvcExtensions;
+
+[assembly: TypeForwardedTo(typeof(SelectListActionAttribute))]
+[assembly: TypeForwardedTo(typeof(AbstractRemoteValidationConfigurator<>))]
+[assembly: TypeForwardedTo(typeof(IRemoteValidationConfigurator<>))]
+[assembly: TypeForwardedTo(typeof(RemoteValidationConfigurator<>))]
+[assembly: TypeForwardedTo(typeof(RemoteValidationExtensions))]
+[assembly: TypeForwardedTo(typeof(RemoteValidationMetadata))]
+[assembly: TypeForwardedTo(typeof(CompareValidationMetadata))]
+[assembly: TypeForwardedTo(typeof(CustomValidationMetadata<>))]
+[assembly: TypeForwardedTo(typeof(DelegateBasedValidator))]
+[assembly: TypeForwardedTo(typeof(ExtendedModelMetadata))]
+[assembly: TypeForwardedTo(typeof(HtmlSelectModelMetadataItemBuilderExtensions))]
+[assembly: TypeForwardedTo(typeof(IModelMetadataAdditionalSetting))]
+[assembly: TypeForwardedTo(typeof(IModelMetadataConfiguration))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataConfiguration<>))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataItem))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataItemBuilder<>))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataItemBuilderRenderActionExtensions))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataItemExtensions))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataItemSelectableElementSetting))]
+[assembly: TypeForwardedTo(typeof(ModelValidationMetadata))]
+[assembly: TypeForwardedTo(typeof(RangeMetadataItemBuilderExtensions))]
+[assembly: TypeForwardedTo(typeof(RangeValidationMetadata<>))]
+[assembly: TypeForwardedTo(typeof(RegularExpressionValidationMetadata))]
+[assembly: TypeForwardedTo(typeof(RenderActionSetting))]
+[assembly: TypeForwardedTo(typeof(RenderActionSettingExtensions))]
+[assembly: TypeForwardedTo(typeof(RequiredValidationMetadata))]
+[assembly: TypeForwardedTo(typeof(StringLengthValidationMetadata))]
+[assembly: TypeForwardedTo(typeof(StringMetadataItemBuilder))]
+[assembly: TypeForwardedTo(typeof(ValueTypeMetadataItemBuilderExtensions))]
+[assembly: TypeForwardedTo(typeof(CompositeModelValidatorProvider))]
+[assembly: TypeForwardedTo(typeof(ExtendedModelMetadataProvider))]
+[assembly: TypeForwardedTo(typeof(IModelMetadataRegistry))]
+[assembly: TypeForwardedTo(typeof(ModelMetadataRegistry))]
