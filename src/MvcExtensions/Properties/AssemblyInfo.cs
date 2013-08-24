e851f395075c834bed1990f331a0c247bd2d80d2
Alexander I. Zaytsev <hazzik@gmail.com>
2013-08-24 23:29:36 +1200

    Increment version to 2.5.2
    AssemblyVersion remains 2.5.1

--- src/MvcExtensions/Properties/AssemblyInfo.cs
+++ src/MvcExtensions/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MvcExtensions 2.0")]
-[assembly: AssemblyProduct("MvcExtensions 2.0")]
+[assembly: AssemblyTitle("MvcExtensions 2.5.2")]
+[assembly: AssemblyProduct("MvcExtensions 2.5.2")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("c16bbd53-b25e-42e5-b75b-f9132846a59c")]
 [assembly: NeutralResourcesLanguage("en-US")]
